3f0ab58b10ba63b9273269c5bfc123da870f3c3b
Johan Lund <133656086+lund97johan@users.noreply.github.com>
2024-04-12 07:45:26 +0000

    Contact Form finished.

--- client/my-app/src/Sites/ContactUsSite/ContactUs.js
+++ client/my-app/src/Sites/ContactUsSite/ContactUs.js
@@ -1,20 +1,33 @@
 import * as React from "react";
+import ContactForm from "./ContactForm";
 import "./ContactUs.css";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {ReturnHeader} from "../../App";
-import {ReturnFooter} from "../../App";
+import { ReturnHeader } from "../../App";
+import { ReturnFooter } from "../../App";
 
-function ContactUs(){
-    return(
+/*function ContactUs() {
+    return (
         <div className='App'>
-            <ReturnHeader/>
-              <div className='App-body'>
-               <text className="ContactUsText" style={{color: "white", fontSize: 50}}>PLEASE SEND US ALL THE EMAILS TO jestru-2@student.ltu.se</text>
+            <ReturnHeader />
+            <div className='App-body'>
+                <text className="ContactUsText" style={{ color: "white", fontSize: 50 }}>PLEASE SEND US ALL THE EMAILS TO jestru-2@student.ltu.se</text>
             </div>
-            <ReturnFooter/>
+            <ReturnFooter />
         </div>
     )
+} */
+
+function ContactUs() {
+    return (
+        <div className='App'>
+            <ReturnHeader />
+            <div className='App-body'>
+                <ContactForm />
+            </div>
+            <ReturnFooter />
+        </div>
+    );
 }
 
 export default ContactUs;
